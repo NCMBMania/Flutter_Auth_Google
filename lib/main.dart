522711a4569b16ab5ecd55ecfa3d94d59cf7a547
Atsushi <atsushi@moongift.jp>
2022-05-09 15:14:35 +0900

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:twitter_login/twitter_login.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:ncmb/ncmb.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   // NCMBの初期化
@@ -32,12 +33,6 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   NCMBUser? _user;
-  // TwitterのConsumer Key（APIキー）
-  final _apiKey = 'YOUR_TWITTER_API_KEY';
-  // TwitterのConsumer Secret Key（APIシークレットキー）
-  final _apiSecretKey = 'YOUR_TWITTER_API_SECRET_KEY';
-  // アプリのURIスキーム
-  final _sheme = 'ncmbauth://';
 
   @override
   void initState() {
@@ -56,13 +51,13 @@ class _MyHomePageState extends State<MyHomePage> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Twitter Login App'),
+          title: const Text('Facebook Login App'),
         ),
         body: Center(
             child: _user == null
                 // 未ログインの場合
                 ? TextButton(
-                    child: const Text('Login With Twitter'),
+                    child: const Text('Login With Facebook'),
                     onPressed: login,
                   )
                 // ログインしている場合
@@ -85,48 +80,34 @@ class _MyHomePageState extends State<MyHomePage> {
 
   // ログイン処理
   login() async {
-    // Twitterでのログイン情報
-    final twitterLogin = TwitterLogin(
-      apiKey: _apiKey,
-      apiSecretKey: _apiSecretKey,
-      redirectURI: _sheme,
-    );
-    // ログインを実行して結果を受け取る
-    final authResult = await twitterLogin.login();
-    switch (authResult.status!) {
-      case TwitterLoginStatus.loggedIn:
-        // ログイン成功
-        // ユーザーデータは繰り返し使うので取り出す
-        final userData = authResult.user!;
-        // 認証用データの作成
-        final data = {
-          'oauth_token': authResult.authToken,
-          'oauth_token_secret': authResult.authTokenSecret,
-          'oauth_consumer_key': _apiKey,
-          'consumer_secret': _apiSecretKey,
-          'id': userData.id.toString(), // 文字列で指定します
-          'screen_name': userData.screenName,
-        };
-        // ログイン実行
-        var user = await NCMBUser.loginWith('twitter', data);
-        // 表示名を追加
-        user.set('displayName', userData.screenName);
-        // 更新実行
-        await user.save();
-        // 表示に反映
-        setState(() {
-          _user = user;
-        });
-        // success
-        break;
-      case TwitterLoginStatus.cancelledByUser:
-        // 認証をキャンセルした場合
-        debugPrint('TwitterLoginStatus.cancelledByUser');
-        break;
-      case TwitterLoginStatus.error:
-        // 認証でエラーが発生した場合
-        debugPrint('TwitterLoginStatus.error');
-        break;
+    final result = await FacebookAuth.instance.login();
+    if (result.status == LoginStatus.success) {
+      final now = DateTime.now();
+      final expirationDate = now.add(const Duration(days: 30));
+      final userData = await FacebookAuth.instance.getUserData();
+      final format = DateFormat("yyyy-MM-ddTHH:mm:ss.S'Z'");
+      // リクエストデータの組立
+      final data = {
+        'id': userData['id'].toString(),
+        'access_token': result.accessToken!.token,
+        'expiration_date': {
+          '__type': 'Date',
+          'iso': format.format(expirationDate)
+        }
+      };
+      // ログイン実行
+      var user = await NCMBUser.loginWith('facebook', data);
+      // 表示名を追加
+      user.set('displayName', userData['name']);
+      // 更新実行
+      await user.save();
+      // 表示に反映
+      setState(() {
+        _user = user;
+      });
+    } else {
+      debugPrint(result.status.toString());
+      debugPrint(result.message);
     }
   }
 }
