0c309b727e70a53aca69e7d1901057236171ea59
Atsushi <atsushi@moongift.jp>
2022-05-09 13:49:12 +0900

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:twitter_login/twitter_login.dart';
+import 'package:ncmb/ncmb.dart';
 
 void main() {
+  // NCMBの初期化
+  NCMB('YOUR_APPLICATION_KEY', 'YOUR_CLIENT_KEY');
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -31,16 +24,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -48,68 +31,102 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  NCMBUser? _user;
+  // TwitterのConsumer Key（APIキー）
+  final _apiKey = 'YOUR_TWITTER_API_KEY';
+  // TwitterのConsumer Secret Key（APIシークレットキー）
+  final _apiSecretKey = 'YOUR_TWITTER_API_SECRET_KEY';
+  // アプリのURIスキーム
+  final _sheme = 'ncmbauth://';
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  @override
+  void initState() {
+    super.initState();
+    Future(() async {
+      // 現在ログインしているユーザー情報（未ログインの場合はnull）を取得
+      final user = await NCMBUser.currentUser();
+      setState(() {
+        _user = user;
+      });
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Twitter Login App'),
         ),
+        body: Center(
+            child: _user == null
+                // 未ログインの場合
+                ? TextButton(
+                    child: const Text('Login With Twitter'),
+                    onPressed: login,
+                  )
+                // ログインしている場合
+                : TextButton(
+                    child: Text(
+                        'Logged in by ${_user!.getString('displayName', defaultValue: 'No name')}'),
+                    onPressed: logout,
+                  )),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  // ログアウト処理
+  logout() async {
+    await NCMBUser.logout();
+    setState(() {
+      _user = null;
+    });
+  }
+
+  // ログイン処理
+  login() async {
+    // Twitterでのログイン情報
+    final twitterLogin = TwitterLogin(
+      apiKey: _apiKey,
+      apiSecretKey: _apiSecretKey,
+      redirectURI: _sheme,
+    );
+    // ログインを実行して結果を受け取る
+    final authResult = await twitterLogin.login();
+    switch (authResult.status!) {
+      case TwitterLoginStatus.loggedIn:
+        // ログイン成功
+        // ユーザーデータは繰り返し使うので取り出す
+        final userData = authResult.user!;
+        // 認証用データの作成
+        final data = {
+          'oauth_token': authResult.authToken,
+          'oauth_token_secret': authResult.authTokenSecret,
+          'oauth_consumer_key': _apiKey,
+          'consumer_secret': _apiSecretKey,
+          'id': userData.id.toString(), // 文字列で指定します
+          'screen_name': userData.screenName,
+        };
+        // ログイン実行
+        var user = await NCMBUser.loginWith('twitter', data);
+        // 表示名を追加
+        user.set('displayName', userData.screenName);
+        // 更新実行
+        await user.save();
+        // 表示に反映
+        setState(() {
+          _user = user;
+        });
+        // success
+        break;
+      case TwitterLoginStatus.cancelledByUser:
+        // 認証をキャンセルした場合
+        debugPrint('TwitterLoginStatus.cancelledByUser');
+        break;
+      case TwitterLoginStatus.error:
+        // 認証でエラーが発生した場合
+        debugPrint('TwitterLoginStatus.error');
+        break;
+    }
   }
 }
