9e6836fddd492196f7c7dde5f46e5521ebbe4b5a
Atsushi <atsushi@moongift.jp>
2022-05-10 11:10:46 +0900

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:twitter_login/twitter_login.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:ncmb/ncmb.dart';
 
 void main() {
@@ -32,12 +32,6 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   NCMBUser? _user;
-  // TwitterのConsumer Key（APIキー）
-  final _apiKey = 'YOUR_TWITTER_API_KEY';
-  // TwitterのConsumer Secret Key（APIシークレットキー）
-  final _apiSecretKey = 'YOUR_TWITTER_API_SECRET_KEY';
-  // アプリのURIスキーム
-  final _sheme = 'ncmbauth://';
 
   @override
   void initState() {
@@ -56,13 +50,13 @@ class _MyHomePageState extends State<MyHomePage> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Twitter Login App'),
+          title: const Text('Google Login App'),
         ),
         body: Center(
             child: _user == null
                 // 未ログインの場合
                 ? TextButton(
-                    child: const Text('Login With Twitter'),
+                    child: const Text('Login With Google'),
                     onPressed: login,
                   )
                 // ログインしている場合
@@ -85,48 +79,28 @@ class _MyHomePageState extends State<MyHomePage> {
 
   // ログイン処理
   login() async {
-    // Twitterでのログイン情報
-    final twitterLogin = TwitterLogin(
-      apiKey: _apiKey,
-      apiSecretKey: _apiSecretKey,
-      redirectURI: _sheme,
+    final googleSignIn = GoogleSignIn(
+      scopes: <String>[
+        'email',
+      ],
     );
     // ログインを実行して結果を受け取る
-    final authResult = await twitterLogin.login();
-    switch (authResult.status!) {
-      case TwitterLoginStatus.loggedIn:
-        // ログイン成功
-        // ユーザーデータは繰り返し使うので取り出す
-        final userData = authResult.user!;
-        // 認証用データの作成
-        final data = {
-          'oauth_token': authResult.authToken,
-          'oauth_token_secret': authResult.authTokenSecret,
-          'oauth_consumer_key': _apiKey,
-          'consumer_secret': _apiSecretKey,
-          'id': userData.id.toString(), // 文字列で指定します
-          'screen_name': userData.screenName,
-        };
-        // ログイン実行
-        var user = await NCMBUser.loginWith('twitter', data);
-        // 表示名を追加
-        user.set('displayName', userData.screenName);
-        // 更新実行
-        await user.save();
-        // 表示に反映
-        setState(() {
-          _user = user;
-        });
-        // success
-        break;
-      case TwitterLoginStatus.cancelledByUser:
-        // 認証をキャンセルした場合
-        debugPrint('TwitterLoginStatus.cancelledByUser');
-        break;
-      case TwitterLoginStatus.error:
-        // 認証でエラーが発生した場合
-        debugPrint('TwitterLoginStatus.error');
-        break;
+    final account = await googleSignIn.signIn();
+    if (account == null) return;
+    final accessToken = (await account.authentication).accessToken;
+    // 認証用データの作成
+    final data = {'id': account.id, 'access_token': accessToken};
+    // ログイン実行
+    var user = await NCMBUser.loginWith('google', data);
+    // 表示名を追加
+    if (user.getString('displayName', defaultValue: '') == '') {
+      user.set('displayName', account.displayName!);
+      // 更新実行
+      await user.save();
     }
+    // 表示に反映
+    setState(() {
+      _user = user;
+    });
   }
 }
